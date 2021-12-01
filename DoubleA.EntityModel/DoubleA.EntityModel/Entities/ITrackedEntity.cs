be151c19efbe27e925dbfefbb7999492de6386dc
Anwar Ahmed <anwar.ahmed@gmail.com>
2021-12-01 00:20:10 -0500

    new interfaces and abstract classes (#5)
    Co-authored-by: Anwar Ahmed <anwar.ahmed+retail@gmail.com>

---
+++ DoubleA.EntityModel/DoubleA.EntityModel/Entities/ITrackedEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DoubleA.EntityModel.Entities
+{
+    public interface ITrackedEntity
+    {
+        DateTime Created { get; set; }
+
+        DateTime Updated { get; set; }
+    }
+}
